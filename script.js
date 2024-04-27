35732d159127afea6cf6a3a581c4684374e012d9
omer- <okisakol@gmail.com>
2024-04-27 21:11:42 +0300

    script

--- script.js
+++ script.js
@@ -2,17 +2,32 @@
 //
 // console.log(data);
 
+const btn_weekly = document.getElementById('weekly');
+const btn_daily = document.getElementById('daily');
+const btn_monthly = document.getElementById('monthly');
+
+
 const work1 = document.getElementById('work-p')
+const work2 = document.getElementById('work-span')
+
+
+
 
 let data = fetch('data.json')
 .then(response => response.json())
 .then(veri => {
     veri.forEach(element => {
-         if (element.title === 'Work'){
-             work1.innerHTML += `${element.timeframes.weekly.current}`
-             console.log(element.timeframes.weekly.current);
-             console.log(element.timeframes.weekly.previous);
-         }
+
+        btn_weekly.addEventListener("click", function () {
+            if (element.title === 'Work' && work1.innerHTML === ''){
+                work1.innerHTML += `${element.timeframes.weekly.current}hrs`
+            }
+            if (element.title === 'Work' && work2.innerHTML === ''){
+                work2.innerHTML += `Last Week - ${element.timeframes.weekly.previous}hrs`
+            }
+        });
+
+
 
     });
 });
